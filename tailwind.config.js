2b3d07512034021a2e67481397985f16ab46712e
1koder <aderohunbi.a.a@gmail.com>
2023-01-02 10:01:45 +0100

    Mobile Version demo

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-      './pages/**/*.{html,js}',
-      './components/**/*.{html,js}',
-      './src/**/*.html',
-      './src/**/*.js',
-      './public/**/*.html',
-      './public/**/*.js'
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js}',
+    './src/**/*.html',
+    './src/**/*.js',
+    './public/**/*.html',
+    './public/**/*.js'
   ],
   theme: {
     extend: {
       backgroundImage: {
         'hero-pattern': "url('/images/stone.svg')",
         'footer-texture': "url('/img/footer-texture.png')",
-      }
+      },
+      screens: {
+        "sm": "480px",
+        "md": "790px",
+        "lg": "1024px",
+      },
     },
   },
   plugins: [],
